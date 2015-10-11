198bebd2f9db12831192c1d8a95ec6941e72c51f
Damianekk <damian.budzynowski@gmail.com>
2015-10-11 23:13:40 +0200

    Nowa klasa recepcjonista

---
+++ Silownia/Silownia/Models/Recepcjonista.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Silownia.Models
+{
+    public class Recepcjonista : Pracownik
+    {
+        public virtual ICollection<Umowa> Umowa { get; set; }
+    }
+}
