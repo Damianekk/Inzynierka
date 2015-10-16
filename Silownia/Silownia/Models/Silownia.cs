83808d524ba77746df4ebcaec8a418a9dc2ebaa7
Damianekk <damian.budzynowski@gmail.com>
2015-10-16 14:14:07 +0200

    Dodanie wyświetlania poprawnych nazw

--- Silownia/Silownia/Models/Silownia.cs
+++ Silownia/Silownia/Models/Silownia.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Silownia.Models
 {
     public class Silownia 
     {
         public long SilowniaID { get; set; }
         public string Nazwa { get; set; }
+        [Display(Name = "Godziny otwarcia")]
         public string GodzinaOtwarcia { get; set; }
+        [Display(Name = "Godziny zamknięcia")]
         public string GodzinaZamkniecia { get; set; }
         public string Powierzchnia { get; set; }
+        [Display(Name = "Nr kontaktowy")]
         public long NrTelefonu { get; set; }
 
         
