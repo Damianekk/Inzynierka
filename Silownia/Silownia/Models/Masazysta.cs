434c031ee4a604f00c8f16a1c7250f939a251224
olieb <s10377@pjwstk.edu.pl>
2015-10-26 21:54:19 +0100

    s

---
+++ Silownia/Silownia/Models/Masazysta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Silownia.Models
+{
+    public class Masazysta : Pracownik
+    {
+        Masazysta()
+        {
+            Masaze = new List<Masaz>();
+        }
+
+        public virtual IList<Masaz> Masaze { get; set; }
+    }
+}
