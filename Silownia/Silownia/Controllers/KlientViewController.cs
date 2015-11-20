dcf12edf3473f5cb46291f617daf3f85fb5f24f6
Damianekk <damian.budzynowski@gmail.com>
2015-11-20 21:36:50 +0100

    Widok od strony klienta itp

---
+++ Silownia/Silownia/Controllers/KlientViewController.cs
@@ -0,0 +1,31 @@
+﻿using Silownia.DAL;
+using Silownia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Silownia.Controllers
+{
+    public class KlientViewController : Controller
+    {
+        private SilowniaContext db = new SilowniaContext();
+        public ActionResult Index(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Klient klient = db.Klienci.Find(id);
+            
+            if (klient == null)
+            {
+                return HttpNotFound();
+            }
+            var z = klient;
+            return View(z);
+        }
+    }
+}
